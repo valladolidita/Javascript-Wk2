fa1f62c11627a658ccbd17cbd258010babae11cf
valladolidita <kelly@Kellys-MacBook-Pro.local>
2015-09-07 11:12:06 -0500

    Javascript Wk2 Assignment2

---
+++ assignment2/js/main.js
@@ -0,0 +1,35 @@
+var alabama = document.getElementById('alabama');
+var ourFunction = function(){
+    alert('Montgomery');
+};
+alabama.addEventListener('mouseenter',ourFunction);
+
+var alaska = document.getElementById('alaska');
+var ourFunction = function(){
+    alert('Juneau');
+};
+alaska.addEventListener('mouseenter',ourFunction);
+
+var arizona = document.getElementById('arizona');
+var ourFunction = function(){
+    alert('Phoenix');
+};
+arizona.addEventListener('mouseenter',ourFunction);
+
+var arkansas = document.getElementById('arkansas');
+var ourFunction = function(){
+    alert('Little Rock');
+};
+arkansas.addEventListener('mouseenter',ourFunction);
+
+var california = document.getElementById('california');
+var ourFunction = function(){
+    alert('Sacramento');
+};
+california.addEventListener('mouseenter',ourFunction);
+
+var colorado = document.getElementById('colorado');
+var ourFunction = function(){
+    alert('Denver');
+};
+colorado.addEventListener('mouseenter',ourFunction);
